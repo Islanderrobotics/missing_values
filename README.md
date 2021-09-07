0813fc1f0e606019a6a056a38afec9a03a91dd71
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-09-07 15:29:26 -0400

    made those changes to the files

---
+++ README.md
@@ -0,0 +1,4 @@
+this script is to show what is going on under the hood 
+of the python package of IslanderDataPreprocessing MissingValues
+module. In the video ??? on the youtube channel Islander Robotics 
+I use this script to all explain how the simple imputer works.
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import IslanderDataPreprocessing as IR
 from sklearn.impute import SimpleImputer
 data  = pd.read_csv("/Users/williammckeon/Sync/youtube videos/dataanalysis/housing.csv")
 encoder = IR.Encoder(df = data)
-encoder.Check()
+data = encoder.Check()
 impute = SimpleImputer(strategy="median")
-new_impute = impute.fit_transform(encoder.df)
+new_impute = impute.fit_transform(data)
 new_data = pd.DataFrame(new_impute, columns= encoder.df.columns, index= encoder.df.index)
 IR.DataDiscovery(df = new_data)
