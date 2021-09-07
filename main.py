2d26f64b7993333418ac3efd893870f4d7d6dbe3
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-09-07 11:14:42 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import IslanderDataPreprocessing as IR
+from sklearn.impute import SimpleImputer
+data  = pd.read_csv("/Users/williammckeon/Sync/youtube videos/dataanalysis/housing.csv")
+encoder = IR.Encoder(df = data)
+encoder.Check()
+impute = SimpleImputer(strategy="median")
+new_impute = impute.fit_transform(encoder.df)
+new_data = pd.DataFrame(new_impute, columns= encoder.df.columns, index= encoder.df.index)
+IR.DataDiscovery(df = new_data)
